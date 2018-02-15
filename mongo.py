11512b9fc186327faba6aa8adc8ed1f7f9782e92
sonalparab <sparab@stuy.edu>
2018-02-14 22:15:05 -0500

    Finish

--- mongo.py
+++ mongo.py
@@ -4,8 +4,8 @@ connection = pymongo.MongoClient('homer.stuy.edu')
 db = connection.test
 collection = db.restaurants
 
-def findByBorough(bor):
-    for entry in collection.find({'borough': bor}):
+def findByBorough(borough):
+    for entry in collection.find({'borough': borough}):
         print entry
 
 def findByZip(zipcode):
@@ -17,15 +17,15 @@ def findByZipandGrade(zipcode,grade):
         print entry                        
 
 def findByZipandScore(zipcode,score):
-    for entry in collection.find({'address.zipcode' : zipcode, "grades.score": {"$lt": score } }):
+    for entry in collection.find({'address.zipcode' : zipcode, "grades.score": {"$lt": score }}):
         print entry                        
 
 def findByZipandCuisine(zipcode,cuisine):
-    for entry in collection.find({'address.zipcode' : zipcode, "cuisine":cuisine }):
+    for entry in collection.find({'address.zipcode' : zipcode, "cuisine": cuisine }):
         print entry
         
-#findByBorough('Brooklyn')
-#findByZip('10282')
-#findByZipandGrade('10282','A')
-#findByZipandScore('10282',10)
+findByBorough('Brooklyn')
+findByZip('10282')
+findByZipandGrade('10282','A')
+findByZipandScore('10282',10)
 findByZipandCuisine('10282','American')
